3f2807b345b1aaaf5777bc1dad08f6f7a2038d91
Esteban Alvarez <u202315628@upc.edu.pe>
2025-11-24 21:36:27 -0500

    Fix2

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   // Configura el publicPath dependiendo del entorno
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Frontend-AgriLink/'  // Para GitHub Pages
-    : '/',                    // Para desarrollo local
   
+publicPath: process.env.NODE_ENV === 'production'
+  ? '/pruebaagri/'  // ← A esto
+  : '/',
+
   transpileDependencies: true,
   
   // Configuración para desarrollo
